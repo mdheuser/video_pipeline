5d12507f1ba4cdd15b81ffcfaa4d3a829ffe1947
Martin Dahlström-Heuser <martin.heuser@gmail.com>
2026-01-30 17:23:12 +0200

    typos

--- steps/1-extract_frames.sh
+++ steps/1-extract_frames.sh
@@ -1,6 +1,6 @@
 #this program will enter the folder where the videos are stored and extract frames from all videos
 
-cd /Volumes/MOTHERSHIP/NEW-two-min-porn-2025
+cd /Volumes/MOTHERSHIP/NEW-xxxx-2025
 for video in *.avi *.mp4 *.mkv; do
     output_folder="/Users/mdh/Documents/xxxx-2025/frames-2mp"
     mkdir -p "$output_folder"
